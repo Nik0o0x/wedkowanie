f0197a39e5e0a6c1056d14f923d1d9a182223754
Nikodem <nikodem.redmann@wp.pl>
2023-09-18 11:47:51 +0200

    essa

---
+++ wedkuj.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+
+<body>
+    <section id="kolor" class="banner">
+        <h1>Portal dla wędkarzy</h1>
+    </section>
+    <div class="flex">
+        <section class="lewy1">
+            <h3>Ryby zamieszkujące rzeki</h3>
+            <ol>
+                <li>Szczupak król polskich wód</li>
+                <li>Węgorz pospolity nakur**a prądem</li>
+            </ol>
+        </section>
+
+        <section class="lewy2">
+            <h4>Ryby drapieżne naszych wód</h4>
+            <table class="table">
+                <tr>
+                    <th>L.p.</th>
+                    <th>Gatunek</th>
+                    <th>Występowanie</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Szczupak</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Flondra</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Węgorz</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Płotka</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>Karaś</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td>Salamandra</td>
+                    <td></td>
+                </tr>
+            </table>
+        </section>
+
+        <section class="prawy">
+            <img src="ryba1.jpg" alt="Sum" class="zdj">
+            <br>
+            <br><a href="kwerendy.txt">Pobierz kwerendy</a></br>
+        </section>
+    </div>
+    <footer id="kolor" class="stopka">
+        <p>Stronę wykonał: Redmann</p>
+    </footer>
+</body>
+
+</html>
